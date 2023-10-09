ab348373917db30732ffd347fd5841af159a82b8
khanchandanivikas <khanchandani58@gmail.com>
2023-10-09 15:10:59 +0100

    added suspense and await

--- app/routes/recepies._index.jsx
+++ app/routes/recepies._index.jsx
@@ -1,4 +1,5 @@
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, Await } from "@remix-run/react";
+import { Suspense } from 'react';
 import { json } from '@remix-run/node';
 import { getDefaultRecepies } from "../data/recepies.server";
 import ProductCard from "../components/productCard/ProductCard";
@@ -20,11 +21,20 @@ const RecepiesDefault = () => {
   return (
     <section className="container mt-4">
       <div className="row">
-        {recepies.map((recepie) => {
-          return (
-            <ProductCard key={recepie.node.id} link={`/recepie/${recepie.node.id}`} image={recepie.node.mainImage} text={recepie.node.name} />
-          );
-        })}
+        <Suspense fallback={<div>Loading...</div>}>
+          <Await resolve={recepies} errorElement="There was a problem loading products">
+            {(recepies) => recepies.map((recepie) => {
+              return (
+                <ProductCard key={recepie.node.id} link={`/recepie/${recepie.node.id}`} image={recepie.node.mainImage} text={recepie.node.name} />
+              );
+            })}
+            {/* {recepies.map((recepie) => {
+            return (
+              <ProductCard key={recepie.node.id} link={`/recepie/${recepie.node.id}`} image={recepie.node.mainImage} text={recepie.node.name} />
+              );
+            })} */}
+          </Await>
+        </Suspense>
       </div>
     </section>
   );
